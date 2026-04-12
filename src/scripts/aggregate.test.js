6ca3e0db70ce0bb39056a920b8ae028e268616c7
walid-hud <w.oumoulilte@enaa.ma>
2026-04-12 12:58:39 +0100

    docs(diagrams): better use case diagram

--- src/scripts/aggregate.test.js
+++ src/scripts/aggregate.test.js
@@ -1,12 +1,50 @@
-import Supplier from "../modules/supplier/model.js"
-import {connectDB} from "../config/db.js"
+import Supplier from "../modules/supplier/model.js";
+import { connectDB } from "../config/db.js";
+import { Types } from "mongoose";
+await connectDB();
 
-await connectDB()
+const supplierStats = await Supplier.aggregate()
+  .match({ _id: new Types.ObjectId("69d64d51f2888daab91385e0") })
+  .lookup({
+    from: "invoices",
+    localField: "_id",
+    foreignField: "supplierId",
+    as: "invoices",
+  })
+  .lookup({
+    from: "payments",
+    localField: "invoices._id",
+    foreignField: "invoiceId",
+    as: "payments",
+  })
+  .addFields({
+    totalInvoices: { $size: "$invoices" },
+    totalAmount: { $sum: "$invoices.amount" },
+    totalPaid: { $sum: "$payments.amount" },
+    invoicesByStatus: {
+      $arrayToObject: {
+        $map: {
+          input: ["paid", "unpaid", "partially_paid"],
+          as: "status",
+          in: {
+            k: "$$status",
+            v: {
+                $filter: {
+                  input: "$invoices",
+                  as: "invoice",
+                  cond: { $eq: ["$$invoice.status", "$$status"] },
+                },
+            },
+          },
+        },
+      },
+    },
+  })
+  .addFields({
+    totalRemaining: { $subtract: ["$totalAmount", "$totalPaid"] },
+  })
+  .exec();
 
-const supplierStats = await  Supplier
-  .aggregate()
-  .match({})
-  .exec()
+console.log(supplierStats[0].invoicesByStatus);
 
-console.table(supplierStats)
-
+process.exit(0);
